cebf6a266a2bddefa34a48b2242ef3c49b1f553e
budionoturman <budionoturman@gmail.com>
2024-01-19 14:57:25 +0700

    update tambah user notif jika username sudah ada

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -11,31 +11,55 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="name" class="form-label">Nama </label>
-                                <input type="text" class="form-control" name="name" id="name" required>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                    name="name" id="name" value="{{ old('name') }}">
+                                @error('name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="username" class="form-label">username </label>
-                                <input type="text" class="form-control" name="username" id="username" required>
+                                <input type="text" class="form-control @error('username') is-invalid @enderror"
+                                    name="username" id="username" value="{{ old('username') }}">
+                                @error('username')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label">pasword </label>
-                                <input type="text" class="form-control" name="password" id="password" required>
+                                <input type="text" class="form-control" name="password" id="password">
                             </div>
                             <div class="mb-3">
                                 <label for="nip" class="form-label">NIP</label>
-                                <input type="number" class="form-control" name="nip" id="nip">
+                                <input type="number" class="form-control @error('nip') is-invalid @enderror" name="nip"
+                                    id="nip" value="{{ old('nip') }}">
+                                @error('nip')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="no_hp" class="form-label">No. HP</label>
-                                <input type="number" class="form-control" name="no_hp" id="no_hp" required>
+                                <input type="number" class="form-control @error('no_hp') is-invalid @enderror"
+                                    name="no_hp" id="no_hp" value="{{ old('no_hp') }}">
+                                @error('no_hp')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="role_id" class="form-label">Role</label>
                                 <select class="form-control selectrole select2" id="role_id selectrole" name="role_id"
                                     required>
                                     @foreach ($roles as $role)
-                                        @if (old('roles_id') == $role->id)
-                                            <option value="{{ $role->id }}">{{ $role->role_name }}</option>
+                                        @if (old('role_id') == $role->id)
+                                            <option value="{{ $role->id }}" selected>{{ $role->role_name }}</option>
                                         @else
                                             <option value="{{ $role->id }}">{{ $role->role_name }}</option>
                                         @endif
