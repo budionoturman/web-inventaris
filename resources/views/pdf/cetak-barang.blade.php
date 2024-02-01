32fda4d9e7cd5cbf62d8b457702456fbe0f56be8
budionoturman <budionoturman@gmail.com>
2024-02-01 12:59:09 +0700

    [FEATURE] add tanggal cetak barang pdf

--- resources/views/pdf/cetak-barang.blade.php
+++ resources/views/pdf/cetak-barang.blade.php
@@ -1,73 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        #customers {
-            font-family: Arial, Helvetica, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <style>
+            #customers {
+                font-family: Arial, Helvetica, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
 
-        #customers td,
-        #customers th {
-            border: 1px solid #ddd;
-            padding: 8px;
-        }
+            #customers td,
+            #customers th {
+                border: 1px solid #ddd;
+                padding: 8px;
+            }
 
-        #customers tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
+            #customers tr:nth-child(even) {
+                background-color: #f2f2f2;
+            }
 
-        #customers tr:hover {
-            background-color: #ddd;
-        }
+            #customers tr:hover {
+                background-color: #ddd;
+            }
 
-        #customers th {
-            padding-top: 12px;
-            padding-bottom: 12px;
-            text-align: left;
-            background-color: rgba(98, 69, 245, 0.739);
-            color: white;
-        }
-    </style>
-</head>
+            #customers th {
+                padding-top: 12px;
+                padding-bottom: 12px;
+                text-align: left;
+                background-color: rgba(98, 69, 245, 0.739);
+                color: white;
+            }
+        </style>
+    </head>
 
-<body>
+    <body>
 
-    <h3 style="text-align: center; text-transform: uppercase;">Data Barang Inventory Sekolah <br>
-        SMK Avicena Rajeg
-    </h3>
-
-    <table id="customers">
-        <tr>
-            <th>No.</th>
-            <th>Nama barang</th>
-            <th>Kode Barang</th>
-            <th>Jurusan</th>
-            <th>Kondisi</th>
-        </tr>
-        @foreach ($barangs as $data)
+        <h3 style="text-align: center; text-transform: uppercase;">Data Barang Inventory Sekolah <br>
+            SMK Avicena Rajeg <br>
+            {{ Carbon\Carbon::now()->format('d-M-Y') }}
+        </h3>
+        <table id="customers">
             <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $data->barang_name }}</td>
-                <td>{{ $data->barang_code }}</td>
-                <td>{{ $data->jurusan_name }}</td>
-                <td>{{ $data->kondisi }}</td>
+                <th>No.</th>
+                <th>Nama barang</th>
+                <th>Kode Barang</th>
+                <th>Jurusan</th>
+                <th>Kondisi</th>
             </tr>
-        @endforeach
-        <tr>
-            <td>Total Barang</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
-</body>
+            @foreach ($barangs as $data)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $data->barang_name }}</td>
+                    <td>{{ $data->barang_code }}</td>
+                    <td>{{ $data->jurusan_name }}</td>
+                    <td>{{ $data->kondisi }}</td>
+                </tr>
+            @endforeach
+            <tr>
+                <td>Total Barang</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </body>
 
 </html>
